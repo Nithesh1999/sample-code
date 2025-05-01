a5bc0f633438235b07f7c5da7559ee265b9a1dda
Nithesh <somalarajunithesh@email.com>
2025-05-01 21:20:24 +0530

    Updated Reults

--- run_robot.bat
+++ run_robot.bat
@@ -1,13 +1,20 @@
 @echo off
 cd /d %~dp0
 
-if not exist results mkdir results
+:: Clean old results
+if exist results rmdir /s /q results
+mkdir results
 
 echo Starting Robot Framework tests...
 echo Current directory: %CD%
 
-:: Call robot directly by full path
-"C:\Users\somalaraju.nithesh\AppData\Local\Programs\Python\Python313\Scripts\robot.exe" -d results *.robot
+:: Run the correct test file
+"C:\Users\somalaraju.nithesh\AppData\Local\Programs\Python\Python313\Scripts\robot.exe" -d results abc.robot
+
+:: Ensure WORKSPACE path is available
+if "%WORKSPACE%"=="" (
+   set WORKSPACE=C:\ProgramData\Jenkins\.jenkins\workspace\Demo
+)
 
 :: Copy results to Jenkins workspace
 xcopy /Y /E results "%WORKSPACE%\results"
